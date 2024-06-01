b37bd665c40aa9cdf357e8a215a360c737f01edf
Jorge Hernandez Toledo <jorgeht2001@gmail.com>
2024-06-01 13:40:33 +0100

    Gsap update
    Por hoy está bien

--- js/Gsap_Animations.js
+++ js/Gsap_Animations.js
@@ -1,30 +1,30 @@
 
 gsap.registerPlugin(ScrollTrigger);
 
+
 let tarjeta1 = document.getElementById("xp1");
 let tarjeta2 = document.getElementById("xp2");
 let tarjeta3 = document.getElementById("xp3");
 let experienciaLab = document.getElementById("experienciaLab");
+let referencia = document.getElementById("referencia");
 
+// Timeline para la sección experienciaLab
 gsap.timeline({
   scrollTrigger: {
     trigger: experienciaLab,
-    start: "top top", // Inicia cuando la parte superior de #experienciaLab toca la parte superior del viewport
-    end: "+=500", // Duración del pin, ajusta según sea necesario
+    start: "+=100", // Inicia cuando la parte superior de #experienciaLab toca la parte superior del viewport
+    end: "+=1500", // Duración del pin, ajusta según sea necesario
     scrub: true, // Sincroniza la animación con el scroll
     pin: true, // Fija la sección mientras se ejecutan las animaciones
-    markers: true // Opcional: para depuración, muestra los marcadores en la página
   }
 })
-  .to(tarjeta1, {
-    xPercent: -150,
-    duration: 1,
-  })
-  .to(tarjeta2, {
-    xPercent: -150,
-    duration: 1,
-  }, "+=0.5") // Retrasa el inicio de la animación de la segunda tarjeta
-  .to(tarjeta3, {
-    xPercent: -150,
-    duration: 1,
-  }, "+=0.5"); // Retrasa el inicio de la animación de la tercera tarjeta
+.to(tarjeta1, {
+  xPercent: -170,
+  rotate: -45,
+  duration: 3,
+}, "+=2")
+.to(tarjeta2, {
+  xPercent: +160,
+  rotate: 45,
+  duration: 3,
+}, "+=0.5") // Retrasa el inicio de la animación de la segunda tarjeta
